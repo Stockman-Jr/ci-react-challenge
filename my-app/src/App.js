b5a0adad6e3b2ec09f7d385dbec1be8aa34565d4
Stockman-Jr <stockmanmimmi@gmail.com>
2023-01-20 10:50:30 +0000

    list challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import css from './App.module.css';
 //import NavBarSimple from './components/NavBarSimple';
 import NavBarForm from './components/NavBarForm';
 import Sidebar from './components/Sidebar';
+import Content from './components/Content';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <div className={css.App}>
       <Sidebar/>
       <NavBarForm/>
+      <Content />
     </div>
   );
 }
